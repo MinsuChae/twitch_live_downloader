a104b942f13e8f6428499b47151a16bcb6681d82
minsuchae <cmspr90@gmail.com>
2023-08-15 20:38:36 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# 트위치 라이브 다운로더
+##기능
+수행 시 자동으로 해당 스트리머의 방송 시작을 감지하여 자동으로 다운로드 합니다.
+###광고제거 기능
+트위치 터보나 해당 채널 구독자만 가능합니다.
+아래 이미지와 같이 개발자 도구에서 cookies 를 찾아 해당 키값을 복사합니다.
+account.example.json 파일에 각 키에 해당하는 값을 붙여넣고 해당 파일명을 account.json 으로 변경합니다.
+![explain remove ad](https://github.com/MinsuChae/twitch_live_downloader/blob/main/image.png?raw=true)
+###저장 장소
+info.json 파일에서 output 파일을 지정합니다.
+윈도우를 기준으론 파일 탐색기에서 경로를 복사하여 붙여넣은 후 \가 나오는 부분을 \\으로 변경해주시기 바랍니다.
+## 수행방법
+###스탠다드 얼론
+윈도우 기반으로 작성하였습니다.
+크롬 버전은 115 버전을 기준으로 작성하였습니다.
+python 3.11 에서 테스트를 진행하였습니다.
+pip install -e requirements.txt
+을 통해 필요한 라이브러리를 수행하시기 바랍니다.
+python download.py streamer_id
+streamer_id 에 
+###Docker
+우분투를 기반으로 작성하였습니다.
+수행에 필요한 파일들이 전부 있습니다.
+docker build -t twitch_download .
+을 통해 해당 이미지를 빌드합니다.
+
+docker create -e USER=streamer_id --restart=always -v host_path:container_path --name twitch_down twitch_download
+를 통해 컨테이너를 생성합니다.
+streamer_id 에 자동으로 녹화할 스트리머 ID를 넣어주세요.
+host_path 는 리눅스에서 저장할 경로입니다.
+container_path 는 내부 컨테이너에서 사용할 경로입니다. 기본값은 /Download 입니다.
+해당값 변경 시 info.json 파일 값도 변경하셔야 합니다. 
+
+docker start twitch_down
+를 통해 컨테이너를 수행합니다.
+(내부적으로 python 을 foreground로 수행되기 때문에 위와 같이 실행하시는게 편합니다.)
+##사용하는 프로그램 및 라이브러리
+[python](https://www.python.org/)
+[selenium-wire](https://pypi.org/project/selenium-wire/)
+[requests](https://pypi.org/project/requests/)
+[ffmpeg](https://www.ffmpeg.org/)
+[chrome](https://www.google.com/chrome/)
+[chromedriver](https://chromedriver.chromium.org/downloads)
+
+
---
+++ docker_image/download.py
@@ -0,0 +1,119 @@
+import requests
+import re
+import subprocess
+import datetime
+import time
+from seleniumwire import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import logging
+import sys
+import os
+import json
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+
+if len(sys.argv) != 2:
+    print("usage :",sys.argv[0],"id")
+    exit(0)
+else:
+    channel_name = sys.argv[1]
+
+service = Service('/chromedriver')
+options = Options()
+options.add_argument('headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument("disable-gpu")
+
+while True:
+    url = "https://www.twitch.tv/{channel_name}".format(channel_name=channel_name)
+    driver = webdriver.Chrome(service=service, options=options, seleniumwire_options={'ignore_certificate_errors': True})
+    driver.get(url)
+    try:
+        time.sleep(5)
+        element_with_live_time_class = WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.CLASS_NAME, 'live-time')))
+        live_match = True
+    except:
+        live_match = False
+    driver.quit()
+
+    if not live_match:
+        time.sleep(60)
+        continue
+    
+    print('{channel_name} online at {now}'.format(channel_name=channel_name, now=datetime.datetime.now()))
+
+    try:
+        driver = webdriver.Chrome(service=service, options=options, seleniumwire_options={'ignore_certificate_errors': True})
+        if os.path.exists("./account.json"):
+            driver.get('https://www.twitch.tv/directory')
+            account = json.load(open("./account.json"))
+            for key in account.keys():
+                driver.add_cookie({"name":key, "value":account[key],'sameSite':'Lax'})
+            time.sleep(1)
+                
+        driver.get('https://www.twitch.tv/{channel_name}'.format(channel_name=channel_name))
+                
+        time.sleep(5)
+        url = ''
+        for request in driver.requests:
+            if request.response:
+                if request.url.find('.m3u8')>0 and not request.url.endswith('.m3u8'):
+                    url = request.url
+                    break
+
+        driver.quit()
+
+        response2 = requests.get(url)
+        arr = response2.text.strip().split('\n')
+                
+
+        for i in range(len(arr)):
+            if arr[i].startswith('#EXT-X-MEDIA'):
+                find_index = arr[i].find('1080p60')
+                find_index2 = arr[i].find('720p60')
+                if find_index==-1 and find_index2==-1:
+                    continue
+                elif find_index!=-1:
+                    tmp_url = arr[i+1]
+                    if not tmp_url.startswith('http'):
+                        tmp_url = arr[i+2]
+                    break
+                else
+                    tmp_url = arr[i+1]
+                    if not tmp_url.startswith('http'):
+                        tmp_url = arr[i+2]
+                    break
+                    
+
+        stream_url = tmp_url
+        print(stream_url)
+
+
+        # Twitch 스트림을 녹화하여 저장할 파일명
+        now = datetime.datetime.now()
+        if os.path.exists("./info.json"):
+            info = json.load(open("./info.json"))
+        else:
+            info = {}
+            info['output']='.'
+
+        if not os.path.exists('{path}/{channel_name}'.format(path=info['output'],channel_name=channel_name)):
+            os.mkdir('{path}/{channel_name}'.format(path=info['output'],channel_name=channel_name))
+
+        start = time.time()
+        output_file = '{path}/{channel_name}/{now}_output.ts'.format(path=info['output'],channel_name=channel_name,now=now.strftime("%Y-%m-%d %H_%M_%S"))
+        ffmpeg_cmd = 'ffmpeg -i "{stream_url}" -loglevel quiet -c copy "{output_file}"'.format(stream_url=stream_url,output_file=output_file)
+        process = subprocess.Popen(ffmpeg_cmd, shell=True)
+        process.wait()
+        end = time.time()
+        if end - start <= 60:
+            os.remove(output_file)
+    except:
+        pass
